0d7c78c8fee0c8d0de9a60a6dd8d933cc86e094b
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2025-09-25 21:44:29 -0500

    m

--- vm_orchestrator_fase1.sh
+++ vm_orchestrator_fase1.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
 
-#credenciales para acceder a los nodos por ssh
-HOST_IP="10.20.12.187"
+echo "=== Iniciando Orquestador Fase 1 (HeadNode) ==="
+
+# Credenciales
 USER="ubuntu"
 PASSWORD="carlos3105"
 
-#puertos ssh de cada host
-WORKER1_PORT=5802   #server2
-WORKER2_PORT=5803   #server3
-WORKER3_PORT=5804   #server4
-OFS_PORT=5805       #ofs
-HEADNODE_PORT=5801  #server1 
+WORKER1_IP=10.0.10.2   #server2
+WORKER2_IP=10.0.10.3   #server3
+WORKER3_IP=10.0.10.4   #server4
+OFS_IP=10.0.10.5       #ofs
 
-#inicializar Workers
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER1_PORT $USER@$HOST_IP "bash -s" < ./init_worker.sh br-int ens4
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER2_PORT $USER@$HOST_IP "bash -s" < ./init_worker.sh br-int ens4
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER3_PORT $USER@$HOST_IP "bash -s" < ./init_worker.sh br-int ens4
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER1_IP "bash -s" < ./init_worker.sh br-int ens4
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER2_IP "bash -s" < ./init_worker.sh br-int ens4
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER3_IP "bash -s" < ./init_worker.sh br-int ens4
 
-#Inicializar OFS
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $OFS_PORT $USER@$HOST_IP "bash -s" < ./init_ofs.sh br-ofs ens4 ens5 ens6
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$OFS_IP "bash -s" < ./init_ofs.sh br-ofs ens4 ens5 ens6
 
-#3 VMs en cada Worker
 echo "=== Creando VMs en Worker1 (server2) ==="
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER1_PORT $USER@$HOST_IP "bash -s" < ./vm_create.sh vm1 br-int 10 1 20:21:05:35:ee:01
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER1_PORT $USER@$HOST_IP "bash -s" < ./vm_create.sh vm2 br-int 10 2 20:21:05:35:ee:02
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER1_PORT $USER@$HOST_IP "bash -s" < ./vm_create.sh vm3 br-int 10 3 20:21:05:35:ee:03
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER1_IP "bash -s" < ./vm_create.sh vm1 br-int 100 1 20:21:05:35:ee:01
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER1_IP "bash -s" < ./vm_create.sh vm2 br-int 200 2 20:21:05:35:ee:02
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER1_IP "bash -s" < ./vm_create.sh vm3 br-int 300 3 20:21:05:35:ee:03
 
 echo "=== Creando VMs en Worker2 (server3) ==="
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER2_PORT $USER@$HOST_IP "bash -s" < ./vm_create.sh vm4 br-int 10 4 20:21:05:35:ee:04
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER2_PORT $USER@$HOST_IP "bash -s" < ./vm_create.sh vm5 br-int 10 5 20:21:05:35:ee:05
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER2_PORT $USER@$HOST_IP "bash -s" < ./vm_create.sh vm6 br-int 10 6 20:21:05:35:ee:06
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER2_IP "bash -s" < ./vm_create.sh vm4 br-int 100 4 20:21:05:35:ee:04
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER2_IP "bash -s" < ./vm_create.sh vm5 br-int 200 5 20:21:05:35:ee:05
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER2_IP "bash -s" < ./vm_create.sh vm6 br-int 300 6 20:21:05:35:ee:06
 
 echo "=== Creando VMs en Worker3 (server4) ==="
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER3_PORT $USER@$HOST_IP "bash -s" < ./vm_create.sh vm7 br-int 20 7 20:21:05:35:ee:07
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER3_PORT $USER@$HOST_IP "bash -s" < ./vm_create.sh vm8 br-int 20 8 20:21:05:35:ee:08
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $WORKER3_PORT $USER@$HOST_IP "bash -s" < ./vm_create.sh vm9 br-int 20 9 20:21:05:35:ee:09
-
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER3_IP "bash -s" < ./vm_create.sh vm7 br-int 100 7 20:21:05:35:ee:07
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER3_IP "bash -s" < ./vm_create.sh vm8 br-int 200 8 20:21:05:35:ee:08
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$WORKER3_IP "bash -s" < ./vm_create.sh vm9 br-int 300 9 20:21:05:35:ee:09
 
 echo "=== Orquestador fase 1 desplegado ==="
